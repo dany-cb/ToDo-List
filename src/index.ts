7d1521d6907b7efc705e593ca2d8b2e957b590c5
dany-cb <danjes002@gmail.com>
2022-02-11 17:24:18 +0530

    Add functionalities
    Added buttons to delete Tasks and coded their functionalities

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,18 @@ function importAssets() {
   img.src = Icon;
 }
 
+interface btn {
+  elmnt: HTMLButtonElement;
+  hidden: boolean;
+}
+[];
+
+type btnType = "primaryBtns" | "onAddBtns" | "onDelBtns";
+
 class TodoApp {
   btns: {
-    elmnt: HTMLButtonElement;
-    hidden: boolean;
-  }[];
+    [key in btnType]: btn[];
+  };
 
   inputArea: {
     elmnt: HTMLDivElement;
@@ -25,7 +32,11 @@ class TodoApp {
   constructor() {
     importAssets();
 
-    this.btns = [];
+    this.btns = {
+      primaryBtns: [],
+      onAddBtns: [],
+      onDelBtns: []
+    };
     this.updateBtns();
 
     this.inputArea = {
@@ -34,13 +45,41 @@ class TodoApp {
     };
     this.container = document.querySelector(".tasksWrap") as HTMLDivElement;
     this.tasks = [];
-    //  Testing
     this.setListeners();
   }
 
   setListeners() {
-    this.btns[0].elmnt.addEventListener("click", this.toggleStates.bind(this));
-    this.btns[1].elmnt.addEventListener("click", this.onClickSave.bind(this));
+    /*  Primary Buttons */
+    this.btns.primaryBtns[0].elmnt.addEventListener(
+      "click",
+      this.onClickAdd.bind(this)
+    ); // Add Btn
+    this.btns.primaryBtns[1].elmnt.addEventListener(
+      "click",
+      this.onClickDelete.bind(this)
+    ); // Delete Btn
+
+    /*  Buttons shown when Add is clicked */
+    this.btns.onAddBtns[0].elmnt.addEventListener(
+      "click",
+      this.onClickSave.bind(this)
+    ); // Save Btn
+    this.btns.onAddBtns[1].elmnt.addEventListener(
+      "click",
+      this.onClickDiscard.bind(this)
+    ); // Discard Btn
+
+    /*  Buttons shown when Delete is clicked  */
+    this.btns.onDelBtns[0].elmnt.addEventListener(
+      "click",
+      this.onClickDone.bind(this)
+    ); // Done Btn
+    this.btns.onDelBtns[1].elmnt.addEventListener(
+      "click",
+      this.onClickClearAll.bind(this)
+    ); // ClearAll Btn
+
+    /*  Enter key pressed within TextArea */
     this.inputArea.elmnt.addEventListener("keydown", (ev) => {
       if (ev.key === "Enter") {
         // console.log("Enter clicked inside TextBox");
@@ -49,26 +88,40 @@ class TodoApp {
     });
   }
 
-  toggleStates() {
+  toggleBtnStates(btn1: btnType, btn2: btnType) {
+    if (btn1 === btn2)
+      console.warn("toggleBtnStates() called with same arguments");
+
+    [btn1, btn2].forEach((btnType) => {
+      this.btns[btnType].map((btn) => {
+        if (btn.hidden) {
+          btn.elmnt.style.display = "";
+        } else {
+          btn.elmnt.style.display = "none";
+        }
+        btn.hidden = !btn.hidden;
+      });
+    });
+  }
+
+  /**
+   * Toggles the Primary Btns and the InputArea
+   * Can be reused to reverse the changes
+   */
+  onClickAdd() {
     this.inputArea.elmnt.contentEditable = this.inputArea.isEditable
       ? "false"
       : "true";
     this.inputArea.isEditable = !this.inputArea.isEditable;
-    this.btns.map((btn) => {
-      if (btn.hidden) {
-        btn.elmnt.style.display = "";
-      } else {
-        btn.elmnt.style.display = "none";
-      }
-      btn.hidden = !btn.hidden;
-    });
+    this.toggleBtnStates("primaryBtns", "onAddBtns");
+    this.inputArea.elmnt.focus();
   }
 
   onClickSave() {
     if (this.inputArea.elmnt.innerText) {
       this.tasks.push(new Task(this.inputArea.elmnt.innerText, this.container));
       this.inputArea.elmnt.innerText = "";
-      this.toggleStates();
+      this.onClickAdd();
     } else {
       this.inputArea.elmnt.classList.add("addTask-error");
       setTimeout(() => {
@@ -78,15 +131,43 @@ class TodoApp {
     }
   }
 
+  onClickDiscard() {
+    this.inputArea.elmnt.innerText = "";
+    this.onClickAdd();
+  }
+
+  onClickDelete() {
+    this.toggleBtnStates("primaryBtns", "onDelBtns");
+    this.container.classList.toggle("tasksWrap-delete");
+  }
+
+  onClickDone() {
+    this.onClickDelete();
+    this.tasks = this.tasks.filter((task) => !task.isDeleted);
+  }
+
+  onClickClearAll() {
+    this.onClickDelete();
+    this.tasks.map((task) => task.removeTask());
+    this.tasks = [];
+  }
+
   updateBtns() {
-    const btnNames = ["add", "save", "del", "discard"];
-    const hiddenBtns = ["save", "discard"];
-    btnNames.map((name) => {
-      this.btns.push({
-        elmnt: document.querySelector(`.editBtns_${name}`) as HTMLButtonElement,
-        hidden: hiddenBtns.includes(name)
-      });
-    });
+    const btnNames = {
+      primaryBtns: ["add", "del"],
+      onAddBtns: ["save", "discard"],
+      onDelBtns: ["done", "clearAll"]
+    };
+    for (const [key, value] of Object.entries(btnNames)) {
+      this.btns[key as btnType].push(
+        ...value.map((name) => ({
+          elmnt: document.querySelector(
+            `.editBtns_${name}`
+          ) as HTMLButtonElement,
+          hidden: key != "primaryBtns"
+        }))
+      );
+    }
   }
 }
 
--- src/task.ts
+++ src/task.ts
@@ -25,32 +25,60 @@ function addText(taskEl: Node, textData: string) {
   text.className = "task_text";
   text.innerText = textData;
   taskEl.appendChild(text);
+  return text;
 }
 
+function createRemoveBtn() {
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttribute("viewBox", "0 0 24 24");
+  svg.setAttribute("fill", "inherit");
+  svg.setAttribute("class", "i task_delBtn");
+  const use = document.createElementNS("http://www.w3.org/2000/svg", "use");
+  use.setAttribute("href", "#xSymbol");
+  svg.appendChild(use);
+  return svg;
+}
+
+let removeBtn: SVGElement | undefined;
 let div: HTMLDivElement | undefined;
 
 class Task {
-  taskEl: Node;
+  taskEl: HTMLDivElement;
   textEl: HTMLParagraphElement;
   isDone: boolean;
+  isDeleted: boolean;
 
-  constructor(textData: string, container: HTMLDivElement, done = true) {
+  constructor(textData: string, container: HTMLDivElement, done = false) {
     //Defining the necessary structure for each Task and appending it to the container
-    if (!div) {
+    if (!div || !removeBtn) {
       div = createTaskEl();
+      removeBtn = createRemoveBtn();
     }
-    this.taskEl = div.cloneNode(true);
-    addText(this.taskEl, textData);
-    this.textEl = this.taskEl.lastChild as HTMLParagraphElement;
+    this.taskEl = div.cloneNode(true) as HTMLDivElement;
+    this.textEl = addText(this.taskEl, textData);
+    const xBtn = removeBtn.cloneNode(true) as SVGElement;
+    this.taskEl.appendChild(xBtn);
+    container.insertBefore(this.taskEl, container.firstChild);
     const label = this.taskEl.firstChild?.firstChild as HTMLLabelElement;
     label.addEventListener("click", this.toggleState.bind(this));
-    container.appendChild(this.taskEl);
-    this.isDone = done || false;
+    xBtn.addEventListener("click", this.removeTask.bind(this));
+
+    this.isDone = done;
+    this.isDeleted = false;
   }
 
   toggleState() {
     this.isDone = !this.isDone;
-    this.textEl.style.textDecoration = this.isDone ? "" : "line-through";
+    this.textEl.style.textDecoration = this.isDone ? "line-through" : "";
+  }
+
+  removeTask() {
+    this.taskEl.firstChild?.removeEventListener(
+      "click",
+      this.toggleState.bind(this)
+    );
+    this.taskEl.remove();
+    this.isDeleted = true;
   }
 }
 
